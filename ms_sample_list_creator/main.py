a2116ad8c158bcbbe0284d932c0fcd0cb9548a50
edouardbruelhart <edouard.brulhart@gmail.com>
2025-06-06 15:05:46 +0200

    Some cleaning before release

--- ms_sample_list_creator/main.py
+++ ms_sample_list_creator/main.py
@@ -1,10 +1,3 @@
-# Run "pyinstaller --onefile main.py"
-# Generated binaries are made for the native system where the pyinstaller command is run.
-
-# You can generate windows executable from linux using wine, by previously installing wine, python 3.8.20, pyinstaller and
-# other non-built-in packages (here requests and pandas) inside wine. Then run: wine PyInstaller --onefile main.py
-
-# import tkinter as tk
 import requests
 import ttkbootstrap as tb
 
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+# Run "pyinstaller --onefile run.py --hidden-import=PIL._tkinter_finder --hidden-import=PIL.ImageTk"
+# Generated binaries are made for the native system where the pyinstaller command is run.
+
+# You can generate windows executable from linux using wine, by previously installing wine, python 3.8.20, pyinstaller and
+# other non-built-in packages (here requests and pandas) inside wine. Then run: wine PyInstaller --onefile run.py --hidden-import=PIL._tkinter_finder --hidden-import=PIL.ImageTk
+
 from ms_sample_list_creator.main import main
 
 if __name__ == "__main__":
